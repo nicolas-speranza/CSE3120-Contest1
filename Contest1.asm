e65a1c29d3c3caac0f5a60a42ea78e01410793e4
nicolas-speranza <nrinconspera2022@my.fit.edu>
2026-03-07 22:57:27 -0500

    Add files via upload

---
+++ Contest1.asm
@@ -0,0 +1,22 @@
+INCLUDE Irvine32.inc
+
+.data
+titleMsg BYTE "ESCAPE THE SEGFAULT", 0
+startMsg BYTE "Press any key to begin...", 0
+
+.code
+main PROC
+call Clrscr
+
+mov edx, OFFSET titleMsg
+call WriteString
+call Crlf
+
+mov edx, OFFSET startMsg
+call WriteString
+
+call ReadChar
+exit
+main ENDP
+
+END main
