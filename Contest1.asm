351cd079bc9269d9c3a1c5517b53d7afbb552e43
nicolas-speranza <nrinconspera2022@my.fit.edu>
2026-03-28 15:03:59 -0400

    Update Contest1.asm
    Implemented up/down movement (W/S keys) and added a visible border for the game arena

--- Contest1.asm
+++ Contest1.asm
@@ -1,12 +1,24 @@
 INCLUDE Irvine32.inc
 
 .data
-titleMsg BYTE "ESCAPE THE SEGFAULT", 0
-startMsg BYTE "Press any key to begin...", 0
-helpMsg  BYTE "Use A and D to move. Press Q to quit.", 0
-player   BYTE "@", 0
-blank    BYTE " ", 0
-playerX  BYTE 10
+titleMsg    BYTE "ESCAPE THE SEGFAULT", 0
+startMsg    BYTE "Press any key to begin...", 0
+helpMsg     BYTE "Use W A S D to move. Press Q to quit.", 0
+player      BYTE "@", 0
+blank       BYTE " ", 0
+
+playerX     BYTE 10
+playerY     BYTE 5
+
+leftWall    BYTE 5
+rightWall   BYTE 70
+topWall     BYTE 3
+botWall     BYTE 20
+
+leftBound   BYTE 6
+rightBound  BYTE 69
+topBound    BYTE 4
+botBound    BYTE 19
 
 .code
 main PROC
@@ -25,8 +37,10 @@ call Clrscr
 mov edx, OFFSET helpMsg
 call WriteString
 
+call DrawBorder
+
 drawPlayer :
-mov dh, 5
+mov dh, playerY
 mov dl, playerX
 call Gotoxy
 mov edx, OFFSET player
@@ -35,7 +49,7 @@ call WriteString
 gameLoop :
 call ReadKey
 
-mov dh, 5
+mov dh, playerY
 mov dl, playerX
 call Gotoxy
 mov edx, OFFSET blank
@@ -49,6 +63,14 @@ cmp al, 'd'
 je moveRight
 cmp al, 'D'
 je moveRight
+cmp al, 'w'
+je moveUp
+cmp al, 'W'
+je moveUp
+cmp al, 's'
+je moveDown
+cmp al, 'S'
+je moveDown
 cmp al, 'q'
 je quitGame
 cmp al, 'Q'
@@ -56,19 +78,79 @@ je quitGame
 jmp drawPlayer
 
 moveLeft :
-cmp playerX, 0
+mov al, playerX
+cmp al, leftBound
 je drawPlayer
 dec playerX
 jmp drawPlayer
 
 moveRight :
-cmp playerX, 79
+mov al, playerX
+cmp al, rightBound
 je drawPlayer
 inc playerX
+jmp drawPlayer
+
+moveUp :
+mov al, playerY
+cmp al, topBound
+je drawPlayer
+dec playerY
+jmp drawPlayer
+
+moveDown :
+mov al, playerY
+cmp al, botBound
+je drawPlayer
+inc playerY
 jmp drawPlayer
 
 quitGame :
 exit
 main ENDP
+
+DrawBorder PROC
+mov dh, topWall
+mov dl, leftWall
+topLine :
+call Gotoxy
+mov al, '-'
+call WriteChar
+inc dl
+cmp dl, rightWall
+jbe topLine
+
+mov dh, botWall
+mov dl, leftWall
+bottomLine :
+call Gotoxy
+mov al, '-'
+call WriteChar
+inc dl
+cmp dl, rightWall
+jbe bottomLine
+
+mov dl, leftWall
+mov dh, topWall
+leftLine :
+call Gotoxy
+mov al, '|'
+call WriteChar
+inc dh
+cmp dh, botWall
+jbe leftLine
+
+mov dl, rightWall
+mov dh, topWall
+rightLine :
+call Gotoxy
+mov al, '|'
+call WriteChar
+inc dh
+cmp dh, botWall
+jbe rightLine
+
+ret
+DrawBorder ENDP
 
 END main
